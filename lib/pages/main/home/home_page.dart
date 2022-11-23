2a541b093b935886e2d6a97a840476199326a5b3
jungchungsub <cndtjq1248@naver.com>
2022-11-23 19:04:39 +0900

    Scroll 해결하는중

--- lib/pages/main/home/home_page.dart
+++ lib/pages/main/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:finalproject_front/pages/main/home/components/home_body.dart';
+import 'package:finalproject_front/pages/main/home/components/home_header.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -54,88 +56,52 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView(
+      body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              children: [
-                Image.asset(
-                  "assets/home1.jpg",
-                  fit: BoxFit.cover,
-                  height: 100,
-                  width: 350,
-                ),
-                SizedBox(height: 15),
-                _buildRow(),
-                Container(
-                  color: Colors.black12,
-                  height: 2,
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Row _buildRow() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Column(
-          children: [
-            _buildDetailIcon(Icons.bike_scooter),
-            Text("뷰티, 운동"),
-          ],
-        ),
-        Column(
-          children: [
-            _buildDetailIcon(Icons.music_note),
-            Text("댄스, 뮤직"),
-          ],
-        ),
-        Column(
-          children: [
-            _buildDetailIcon(Icons.article_rounded),
-            Text("미술, 묵학"),
-          ],
-        ),
-        Column(
-          children: [
-            _buildDetailIcon(Icons.ac_unit_rounded),
-            Text("공예, 기타"),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildDetailIcon(IconData mIcon) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Stack(
-        children: [
+          HomeHeader(),
           Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(20.0),
-            ),
+            color: Colors.black12,
+            height: 2,
           ),
-          Positioned(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: IconButton(
-                onPressed: () {},
-                icon: Icon(mIcon),
-              ),
-            ),
-          )
+          Expanded(child: _buildTabBarView()),
         ],
       ),
+    );
+  }
+
+  Widget _buildTabBarView() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisSpacing: 10,
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Image.asset("assets/home1.jpg", height: 90),
+                SizedBox(height: 10),
+                Text(
+                  "안녕하세요 저희 서비스에 오셔서 감사합니다. 잘 부탁드립니다",
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: [
+                    Icon(CupertinoIcons.heart_fill, color: Colors.red),
+                    SizedBox(width: 10),
+                    Text(
+                      "4.5 | 25개의 평가",
+                      style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Text("50000원"),
+              ],
+            );
+          }),
     );
   }
 }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:finalproject_front/pages/main/home/home_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -15,6 +16,7 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _buildIndexedStack(),
+      bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
@@ -23,6 +25,23 @@ class _MainPageState extends State<MainPage> {
       index: _selectedIndex, //아이콘클릭시 화면 이동
       children: [
         HomePage(),
+      ],
+    );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      onTap: (index) {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(label: "홈", icon: Icon(CupertinoIcons.home, color: Colors.lightBlue)),
+        BottomNavigationBarItem(label: "검색", icon: Icon(CupertinoIcons.square_on_square, color: Colors.lightBlue)),
+        BottomNavigationBarItem(label: "찜", icon: Icon(CupertinoIcons.placemark, color: Colors.lightBlue)),
+        BottomNavigationBarItem(label: "채팅", icon: Icon(CupertinoIcons.chat_bubble_2, color: Colors.lightBlue)),
+        BottomNavigationBarItem(label: "내정보", icon: Icon(CupertinoIcons.person, color: Colors.lightBlue)),
       ],
     );
   }
